ecde442049adbb4348ea92388e24f0a1bbe86e65
Bhanuka Mallawaarachchi <Bhanukamax@gmail.com>
2021-12-07 10:01:59 +0530

    added home row Enter and Esc

--- keyboards/planck/keymaps/bhanukamax/keymap.c
+++ keyboards/planck/keymaps/bhanukamax/keymap.c
@@ -53,10 +53,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_BASE] = LAYOUT_planck_grid(
-    KC_Q,         KC_W,          KC_E,         KC_R,           KC_T,            XXXXXXX, XXXXXXX,  KC_Y,   KC_U,          KC_I,         KC_O,         KC_P,
-    RCTL_T(KC_A), RSFT_T(KC_S),  RALT_T(KC_D), RGUI_T(KC_F),   KC_G,            XXXXXXX, XXXXXXX,  KC_H,   RGUI_T(KC_J),  RALT_T(KC_K), RSFT_T(KC_L), RCTL_T(KC_SCLN),
+    RCTL_T(KC_Q), KC_W,          KC_E,         KC_R,           KC_T,            XXXXXXX, XXXXXXX,  KC_Y,   KC_U,          KC_I,         KC_O,         KC_P,
+    RCTL_T(KC_A), RSFT_T(KC_S),  RALT_T(KC_D), RGUI_T(KC_F),   KC_G,            XXXXXXX, XXXXXXX,  KC_H,   LGUI_T(KC_J),  LALT_T(KC_K), RSFT_T(KC_L), RCTL_T(KC_SCLN),
     KC_Z,         KC_X,          KC_C,         KC_V,           KC_B,            KC_NO,   KC_NO,    KC_N,   KC_M,          KC_COMM,      KC_DOT,       KC_SLSH,
-    XXXXXXX,       XXXXXXX,       XXXXXXX,      NAV_TAB,        RSFT_T(KC_SPC),  XXXXXXX, XXXXXXX,  NUMBER, KC_RSFT,  XXXXXXX,      XXXXXXX,      XXXXXXX
+    XXXXXXX,      XXXXXXX,       XXXXXXX,      NAV_TAB,        RSFT_T(KC_SPC),  XXXXXXX, XXXXXXX,  NUMBER, KC_RSFT,  XXXXXXX,      XXXXXXX,      XXXXXXX
 ),
 
 [_NUM] = LAYOUT_planck_grid(
@@ -84,8 +84,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 const uint16_t PROGMEM test_combo1[] = {KC_M, KC_COMM, COMBO_END}; // right hand m and comma
 const uint16_t PROGMEM test_combo2[] = {KC_Z, KC_X, COMBO_END}; // Left hand Z + X -> FN
-const uint16_t PROGMEM test_combo3[] = {KC_U, KC_I, COMBO_END}; // NUM layer left hand -> Base
-const uint16_t PROGMEM test_combo4[] = {KC_C, KC_V, COMBO_END}; // left hand -> Base
+const uint16_t PROGMEM test_combo3[] = {LGUI_T(KC_J), LALT_T(KC_K), COMBO_END}; // Enter
+const uint16_t PROGMEM test_combo4[] = {RALT_T(KC_D), RGUI_T(KC_F), COMBO_END}; // right hand -> Esc
 const uint16_t PROGMEM test_combo5[] = {KC_E, KC_R, KC_U, KC_I, COMBO_END}; // left hand -> Base
 
 combo_t key_combos[COMBO_COUNT] = {
