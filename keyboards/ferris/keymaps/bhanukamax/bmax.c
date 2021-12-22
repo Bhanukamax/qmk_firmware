6c13bd80718225345b74137e6ae5b499c57a2d55
Bhanuka Mallawaarachchi <Bhanukamax@gmail.com>
2021-12-22 22:10:26 +0530

    update

--- keyboards/ferris/keymaps/bhanukamax/bmax.c
+++ keyboards/ferris/keymaps/bhanukamax/bmax.c
@@ -2,10 +2,14 @@
 #include "muse.h"
 
 const uint16_t PROGMEM combo_osl_fn[] = {KC_M, KC_COMM, COMBO_END}; // right hand m and comma
+const uint16_t PROGMEM combo_tab[] = {KC_T, KC_G, COMBO_END}; // right hand m and comma
+const uint16_t PROGMEM combo_esc[] = {KC_G, KC_B, COMBO_END}; // right hand m and comma
 const uint16_t PROGMEM combo_reset[] = {KC_E, KC_R, KC_U, KC_I, COMBO_END}; // left hand -> Base
 
 combo_t key_combos[COMBO_COUNT] = {
     COMBO(combo_osl_fn, OSL(4)),
     COMBO(combo_reset, RESET),
+    COMBO(combo_esc, KC_ESC),
+    COMBO(combo_tab, KC_TAB),
 };
 
