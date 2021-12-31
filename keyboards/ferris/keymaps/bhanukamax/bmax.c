e24d75c4be3de765d1f1b2f2a1877f0f64172787
Bhanuka Mallawaarachchi <Bhanukamax@gmail.com>
2021-12-31 13:29:15 +0530

    added two thum space to use tap and hold space

--- keyboards/ferris/keymaps/bhanukamax/bmax.c
+++ keyboards/ferris/keymaps/bhanukamax/bmax.c
@@ -7,6 +7,8 @@ const uint16_t PROGMEM combo_esc[] = {KC_G, KC_B, COMBO_END}; // right hand m an
 const uint16_t PROGMEM combo_reset[] = {KC_E, KC_R, KC_U, KC_I, COMBO_END}; // left hand -> Base
 const uint16_t PROGMEM combo_to_swap_layer[] = {KC_D, KC_F, COMBO_END}; // left hand -> Base
 const uint16_t PROGMEM combo_to_base_layer[] = {KC_J, KC_K, COMBO_END}; // left hand -> Base
+const uint16_t PROGMEM combo_space[] = {RSFT_T(KC_SPC), MO(1), COMBO_END}; // left hand -> Base
+
 
 combo_t key_combos[COMBO_COUNT] = {
     COMBO(combo_osl_fn, OSL(4)),
@@ -15,4 +17,5 @@ combo_t key_combos[COMBO_COUNT] = {
     COMBO(combo_tab, KC_TAB),
     COMBO(combo_to_swap_layer, TO(3)),
     COMBO(combo_to_base_layer, TO(0)),
+    COMBO(combo_space, KC_SPC),
 };
