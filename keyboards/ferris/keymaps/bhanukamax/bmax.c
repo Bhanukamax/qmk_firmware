359cbe1534b67930216a7d57bc8a1af0dc0c9b99
Bhanuka Mallawaarachchi <Bhanukamax@gmail.com>
2022-01-19 16:32:10 +0530

    fix

--- keyboards/ferris/keymaps/bhanukamax/bmax.c
+++ keyboards/ferris/keymaps/bhanukamax/bmax.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 #include "muse.h"
 
-const uint16_t PROGMEM combo_osl_fn[] = {RGUI_T(KC_M), KC_COMM, COMBO_END}; // right hand m and comma
+const uint16_t PROGMEM combo_osl_fn[] = {KC_M, KC_COMM, COMBO_END}; // right hand m and comma
 const uint16_t PROGMEM combo_tab[] = {KC_T, KC_G, COMBO_END}; // right hand m and comma
 const uint16_t PROGMEM combo_esc[] = {KC_G, KC_B, COMBO_END}; // right hand m and comma
 const uint16_t PROGMEM combo_reset[] = {KC_E, KC_R, KC_U, KC_I, COMBO_END}; // left hand -> Base
@@ -14,6 +14,10 @@ const uint16_t PROGMEM combo_space[] = {RSFT_T(KC_SPC), MO(1), COMBO_END}; // le
 //const uint16_t PROGMEM combo_lshift_space[] = {RGUI_T(KC_M), KC_J, COMBO_END}; // left hand -> Base
 const uint16_t PROGMEM combo_rshift_space[] = {KC_R, KC_F, COMBO_END}; // left hand -> Base
 const uint16_t PROGMEM combo_lshift_space[] = {KC_U, KC_J, COMBO_END}; // left hand -> Base
+
+
+const uint16_t PROGMEM combo_rgui[] = {KC_V, KC_F, COMBO_END}; // left hand -> Base
+const uint16_t PROGMEM combo_lgui[] = {KC_M, KC_J, COMBO_END}; // left hand -> Base
 
 //const uint16_t PROGMEM combo_bsp[] = {RCTL_T(KC_SCLN), LALT_T(KC_SLSH), COMBO_END}; // left hand -> Base
 const uint16_t PROGMEM combo_bsp[] = {KC_Y, KC_H, COMBO_END}; // left hand -> Base
@@ -32,6 +36,10 @@ combo_t key_combos[COMBO_COUNT] = {
     COMBO(combo_lshift_space, RSFT_T(KC_SPC)),
 
     COMBO(combo_bsp, KC_BSPC),
+
+
+    COMBO(combo_rgui, KC_RGUI),
+    COMBO(combo_lgui, KC_RGUI),
 };
 
 
