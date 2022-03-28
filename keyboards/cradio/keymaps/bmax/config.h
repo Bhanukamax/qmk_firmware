742feb309a2438ed5d5b138fd0f8fb47ccc804f7
Bhanuka Mallawaarachchi <bhanukamax@gmail.com>
2022-03-28 23:12:52 +0530

    copied mouse valuse from Ben Vallack's config
    https://github.com/benvallack/34-QMK-Ferris-Sweep/blob/main/config.h

--- keyboards/cradio/keymaps/bmax/config.h
+++ keyboards/cradio/keymaps/bmax/config.h
@@ -27,13 +27,16 @@
 
 // Improved mouse key defaults
 // Delay between pressing a key and cursor movement
-#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_DELAY 5
 // Time between cursor movements in milliseconds
-#define MOUSEKEY_INTERVAL 10
+#define MOUSEKEY_INTERVAL 16
 // Step size for acceleration
-#define MOUSEKEY_MOVE_DELTA 8
-#define MOUSEKEY_MAX_SPEED 12
-#define MOUSEKEY_TIME_TO_MAX 200
+#define MOUSEKEY_MOVE_DELTA 1
+#define MOUSEKEY_MAX_SPEED 22
+
+//#define MOUSEKEY_TIME_TO_MAX 200
+#define MOUSEKEY_DECELERATED_SPEED 12
+
 #define MOUSEKEY_WHEEL_DELAY 16
 #define MOUSEKEY_WHEEL_INTERVAL 30
 #define MOUSEKEY_WHEEL_MAX_SPEED 10
