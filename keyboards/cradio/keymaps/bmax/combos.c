a4661602a32ce5fe23b892310ba363fb1441b87d
Bhanuka Mallawaarachchi <bhanukamax@gmail.com>
2022-03-28 22:56:04 +0530

    clean up

--- keyboards/cradio/keymaps/bmax/combos.c
+++ keyboards/cradio/keymaps/bmax/combos.c
@@ -56,11 +56,6 @@ combo_t key_combos[COMBO_COUNT] = {
 };
 
 
-bool is_alt_tab_active = false; // ADD this near the begining of keymap.c
-uint16_t alt_tab_timer = 0;     // we will be using them soon.
-
-
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case RCTL_T(KC_GT):
@@ -69,49 +64,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 return false;        // Return false to ignore further processing of key
             }
             break;
-        case ALT_TAB:
-            if (record->event.pressed) {
-                if (!is_alt_tab_active) {
-                    is_alt_tab_active = true;
-                    register_code(KC_LALT);
-                }
-                alt_tab_timer = timer_read();
-                register_code(KC_TAB);
-            } else {
-                unregister_code(KC_TAB);
-            }
-            break;
-        case ALT_SHIFT_TAB:
-            if (record->event.pressed) {
-                if (!is_alt_tab_active) {
-                    is_alt_tab_active = true;
-                    register_code(KC_LALT);
-                }
-                alt_tab_timer = timer_read();
-                register_code(KC_LSFT);
-                register_code(KC_TAB);
-            } else {
-                unregister_code(KC_LSFT);
-                unregister_code(KC_TAB);
-            }
-            break;
-        case RELEASE_ALT_TAB:
-            if (record->event.pressed) {
-                is_alt_tab_active = false;
-                unregister_code(KC_LALT);
-            }
-            break;
     }
     return true;
-}
-
-void matrix_scan_user(void) { // The very important timer.
-  if (is_alt_tab_active) {
-    if (timer_elapsed(alt_tab_timer) > 1000) {
-      /*unregister_code(KC_LALT);*/
-      is_alt_tab_active = false;
-    }
-  }
 }
 
 
