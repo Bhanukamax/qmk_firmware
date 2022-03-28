042f1e87b524fdd95f90988b8fb8ca92366e7e33
Bhanuka Mallawaarachchi <bhanukamax@gmail.com>
2022-03-28 21:51:16 +0530

    added alt tab and alt shift tab to mouse layer

--- keyboards/cradio/keymaps/bmax/combos.c
+++ keyboards/cradio/keymaps/bmax/combos.c
@@ -1,5 +1,6 @@
 #include QMK_KEYBOARD_H
 #include "muse.h"
+#include "keycodes.h"
 
 const uint16_t PROGMEM combo_osl_fn[] = {KC_M, KC_COMM, COMBO_END}; // right hand m and comma
 const uint16_t PROGMEM combo_tab[] = {KC_T, KC_G, COMBO_END}; // right hand m and comma
@@ -55,6 +56,10 @@ combo_t key_combos[COMBO_COUNT] = {
 };
 
 
+bool is_alt_tab_active = false; // ADD this near the begining of keymap.c
+uint16_t alt_tab_timer = 0;     // we will be using them soon.
+
+
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
@@ -64,6 +69,32 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 return false;        // Return false to ignore further processing of key
             }
             break;
+        case ALT_TAB:
+            if (record->event.pressed) {
+                if (!is_alt_tab_active) {
+                    is_alt_tab_active = true;
+                    register_code(KC_LALT);
+                }
+                alt_tab_timer = timer_read();
+                register_code(KC_TAB);
+            } else {
+                unregister_code(KC_TAB);
+            }
+            break;
+        case ALT_SHIFT_TAB:
+            if (record->event.pressed) {
+                if (!is_alt_tab_active) {
+                    is_alt_tab_active = true;
+                    register_code(KC_LALT);
+                }
+                alt_tab_timer = timer_read();
+                register_code(KC_LSFT);
+                register_code(KC_TAB);
+            } else {
+                unregister_code(KC_LSFT);
+                unregister_code(KC_TAB);
+            }
+            break;
     }
     return true;
 }
@@ -71,8 +102,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
   switch(keycode) {
     case SHIFT_SPACE:
-      return true;
+        return true;
     default:
       return false;
   }
 }
+
+
+
+void matrix_scan_user(void) { // The very important timer.
+  if (is_alt_tab_active) {
+    if (timer_elapsed(alt_tab_timer) > 1000) {
+      unregister_code(KC_LALT);
+      is_alt_tab_active = false;
+    }
+  }
+}
+
