7829dac41f2e89159f980e2b12dd9f9e4ef8c3a3
Bhanuka <bhanukamax@gmail.com>
2022-08-29 20:42:49 +0530

    update sweep qmk to match zmk changes

--- keyboards/cradio/keymaps/bmax/keymap.c
+++ keyboards/cradio/keymaps/bmax/keymap.c
@@ -28,10 +28,10 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(// QWERTY
-            GUI_T(KC_Q), KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, GUI_T(KC_P),
-            RCTL_T(KC_A), KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, RCTL_T(KC_SCLN),
+            GUI_T(KC_Q), KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
+            RCTL_T(KC_A), KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN,
             GUI_T(KC_Z), LALT_T(KC_X), KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, LALT_T(KC_DOT), GUI_T(KC_SLSH),
-            MO(3), SHIFT_SPACE, MO(2), KC_ENT),
+            MO(3), SHIFT_SPACE, MO(2), RCTL_T(KC_ENT)),
 	[1] = LAYOUT( //  colmaksu
             KC_Q, KC_W, KC_F, KC_P, KC_G, KC_J, KC_L, KC_U, KC_Y, KC_SCLN,
             RCTL_T(KC_A), KC_R, KC_S, KC_T, KC_D, KC_H, KC_N, KC_E, KC_I, RCTL_T(KC_O),
