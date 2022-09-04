902ca5acff390ff8d132911cb50a2d5ea2f3ebbf
Bhanuka <bhanukamax@gmail.com>
2022-09-04 07:45:38 +0530

    feat: added cradio bmax-dvorak layout

---
+++ keyboards/cradio/keymaps/bmax-dvorak/keycodes.h
@@ -0,0 +1,11 @@
+#pragma once
+#include QMK_KEYBOARD_H
+
+enum bmax_layers {
+    _QWERTY,
+    _COLEMAK,
+    _NUMBER,
+    _NAV,
+    _FN,
+    _MOUSE
+};
---
+++ keyboards/cradio/keymaps/bmax-dvorak/rules.mk
@@ -0,0 +1,5 @@
+SRC += muse.c
+COMBO_ENABLE = yes
+SWAP_HANDS_ENABLE = yes
+SRC += combos.c
+BOOTLOADER = caterina
