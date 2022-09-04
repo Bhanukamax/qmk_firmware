9d18b733a1e80611733993afc33e3bb2e7275f47
Bhanuka <bhanukamax@gmail.com>
2022-09-04 10:13:24 +0530

    fix

--- keyboards/cradio/keymaps/bmax-dvorak/keymap.c
+++ keyboards/cradio/keymaps/bmax-dvorak/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             GUI_T(KC_QUOT), LALT_T(KC_COMM), KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, RALT_T(KC_L),
             RSFT_T(KC_A), KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, LSFT_T(KC_S),
             GUI_T(KC_SCLN), LALT_T(KC_Q), KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, LALT_T(KC_V), GUI_T(KC_Z),
-                    MO(3), CTL_SPACE, MO(2), LALT_T(KC_ENT)),
+            MO(3), LALT_T(KC_SPC), MO(2), LCTL_T(KC_ENT)),
 	[1] = LAYOUT( //  colmaksu
             KC_Q, KC_W, KC_F, KC_P, KC_G, KC_J, KC_L, KC_U, KC_Y, KC_SCLN,
             RCTL_T(KC_A), KC_R, KC_S, KC_T, KC_D, KC_H, KC_N, KC_E, KC_I, RCTL_T(KC_O),
