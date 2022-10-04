9830feb5d9372c2fed0d9f45e67b2cc2f0ba99c8
Bhanuka <bhanukamax@gmail.com>
2022-10-04 16:52:28 +0530

    moved to enum for layers

--- keyboards/cradio/keymaps/bmax-dvorak/keycodes.h
+++ keyboards/cradio/keymaps/bmax-dvorak/keycodes.h
@@ -2,8 +2,8 @@
 #include QMK_KEYBOARD_H
 
 enum bmax_layers {
-    _QWERTY,
-    _COLEMAK,
+    _DVORAK,
+    _GIMP,
     _NUMBER,
     _NAV,
     _FN,
@@ -28,24 +28,24 @@ enum bmax_layers {
 
 #define OSM_SHFT_GUI OSM(MOD_LSFT|MOD_LGUI)
 
-#define DVORAK 0
-#define GIMP 1
-#define NUMBER 2
-#define NAV 3
-#define FUN 4
+// #define DVORAK 0
+// #define GIMP 1
+// #define _NUMBER 2
+// #define NAV 3
+// #define FUN 4
 
 #define RAISE_NUM LT(2, KC_ESC)
 
 #define TOP_DOT KC_DOT
-#define TOP_C LT(NUMBER, KC_C)
+#define TOP_C LT(_NUMBER, KC_C)
 #define NUM_DOT RCTL_T(KC_DOT)
 
-#define TOP_G LT(NUMBER, KC_G)
-#define TOP_P LT(NUMBER, KC_P)
+#define TOP_G LT(_NUMBER, KC_G)
+#define TOP_P LT(_NUMBER, KC_P)
 #define NUM_COMM KC_COMM
 
-#define NUM_I LT(NUMBER, KC_I)
-#define NUM_R LT(NUMBER, KC_R)
+#define NUM_I LT(_NUMBER, KC_I)
+#define NUM_R LT(_NUMBER, KC_R)
 
 #define NUM_K KC_K
-#define NUM_M LT(NUMBER, KC_M)
+#define NUM_M LT(_NUMBER, KC_M)
