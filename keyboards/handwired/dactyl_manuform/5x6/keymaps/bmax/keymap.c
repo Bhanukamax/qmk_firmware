ccfbb9b1948551b1cc16f958c90b6a5c119e933b
Bhanuka <bhanukamax@gmail.com>
2022-10-09 20:13:19 +0530

    dactyl: added homerow mods

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/bmax/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/bmax/keymap.c
@@ -7,12 +7,23 @@
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
 
+
+#define HM_A LSFT_T(KC_A)
+#define HM_O LCTL_T(KC_O)
+#define HM_E LALT_T(KC_E)
+#define HM_U LGUI_T(KC_U)
+
+#define HM_S RSFT_T(KC_S)
+#define HM_N RCTL_T(KC_N)
+#define HM_T RALT_T(KC_T)
+#define HM_H RGUI_T(KC_H)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x6(
         KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
         KC_TAB , KC_QUOT  , KC_COMM  , KC_DOT  , KC_P  , KC_Y  ,                         KC_F  , KC_G  , KC_C  , KC_R  , KC_L  ,KC_BSPC,
-        KC_LSFT, KC_A  , KC_O  , KC_E  , KC_U  , KC_I  ,                         KC_D  , KC_H  , KC_T  , KC_N  ,KC_S,KC_QUOT,
-        KC_LCTL, KC_SCLN  , KC_Q  , KC_J  , KC_K  ,KC_X  ,                         KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,KC_BSLASH,
+        kc_lsft, hm_a  , hm_o  , hm_e  , hm_u  , kc_i  ,                         KC_D  , HM_H  , HM_T  , HM_N  ,HM_S,KC_QUOT,
+        KC_LCTL, KC_SCLN  , KC_Q  , KC_J  , KC_K  ,KC_X  ,                         KC_B  , KC_M  ,KC_W,KC_V ,KC_Z,KC_BSLASH,
                          KC_LBRC,KC_SPC,                                                       KC_BSPC, KC_ENT,
                                          RAISE,KC_SPC,                        KC_ENT, LOWER,
                                          KC_TAB,KC_HOME,                         KC_END,  KC_DEL,
