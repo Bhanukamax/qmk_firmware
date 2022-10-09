3851cd98059048d1ebe973259ef7e67deed87350
Bhanuka <bhanukamax@gmail.com>
2022-10-09 22:51:57 +0530

    fix

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/bmax/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/bmax/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x6(
         KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
         KC_TAB , KC_QUOT  , KC_COMM  , KC_DOT  , KC_P  , KC_Y  ,                         KC_F  , KC_G  , KC_C  , KC_R  , KC_L  ,KC_BSPC,
-        kc_lsft, hm_a  , hm_o  , hm_e  , hm_u  , kc_i  ,                         KC_D  , HM_H  , HM_T  , HM_N  ,HM_S,KC_QUOT,
+        KC_LSFT, HM_A  , HM_O  , HM_E  , HM_U  , KC_I  ,                         KC_D  , HM_H  , HM_T  , HM_N  ,HM_S,KC_QUOT,
         KC_LCTL, KC_SCLN  , KC_Q  , KC_J  , KC_K  ,KC_X  ,                         KC_B  , KC_M  ,KC_W,KC_V ,KC_Z,KC_BSLASH,
                          KC_LBRC,KC_SPC,                                                       KC_BSPC, KC_ENT,
                                          RAISE,KC_SPC,                        KC_ENT, LOWER,
