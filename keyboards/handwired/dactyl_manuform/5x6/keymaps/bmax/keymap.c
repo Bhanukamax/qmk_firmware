ed2a5d48ebaa644d1a7db047846cf651a832e9e8
Bhanuka <bhanukamax@gmail.com>
2022-10-10 17:08:00 +0530

    dactyl: added the fn layer

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/bmax/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/bmax/keymap.c
@@ -38,11 +38,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                            ),
       [_NAV] = LAYOUT_5x6(
                            // number
-        KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,
-        KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_LBRC,
+        KC_TRNS , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,
+        KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10  ,KC_F11,
         // top
         KC_TAB , KC_QUOT  , KC_COMM  , KC_DOT  , KC_P  , KC_Y  ,
-        KC_F  , KC_G  , KC_C  , KC_R  , KC_L  ,KC_RBRC,
+        KC_HOME, KC_PGDN, KC_PGUP, KC_END  , KC_L  ,KC_F12,
         // Home
         KC_MINS, HM_A  , HM_O  , HM_E  , HM_U  , KC_I  ,
         KC_LEFT, KC_DOWN, KC_UP, KC_RGHT  ,HM_S,KC_SLASH,
