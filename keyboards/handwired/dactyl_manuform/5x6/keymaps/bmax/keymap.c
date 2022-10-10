ee197befe479537ca93b4b748fc80c46e46bebbb
Bhanuka <bhanukamax@gmail.com>
2022-10-10 16:59:07 +0530

    dactyl: added nav layer

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/bmax/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/bmax/keymap.c
@@ -1,12 +1,9 @@
 #include QMK_KEYBOARD_H
 
-#define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
-
-#define RAISE MO(_RAISE)
-#define LOWER MO(_LOWER)
-
+enum layers {
+  _DVORAK,
+  _NAV
+};
 
 #define HM_A LSFT_T(KC_A)
 #define HM_O LCTL_T(KC_O)
@@ -19,7 +16,7 @@
 #define HM_H RGUI_T(KC_H)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_QWERTY] = LAYOUT_5x6(
+    [_DVORAK] = LAYOUT_5x6(
                            // number
         KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,
         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_LBRC,
@@ -34,7 +31,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_B  , KC_M  ,KC_W,KC_V ,KC_Z,KC_BSLASH,
         // thumb upper
         KC_LEFT, KC_RIGHT, KC_ESC,KC_SPC, KC_LBRC, KC_UNDS,
-        KC_UNDS, KC_DEL, KC_BSPC, KC_ENT, KC_UP, KC_DOWN,
+        KC_UNDS, KC_DEL, KC_BSPC, KC_ENT, KC_DOWN, KC_UP,
+        // thumb lower
+        MO(_NAV), KC_MINS,
+        KC_MINS, LCTL_T(KC_DEL)
+                           ),
+      [_NAV] = LAYOUT_5x6(
+                           // number
+        KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,
+        KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_LBRC,
+        // top
+        KC_TAB , KC_QUOT  , KC_COMM  , KC_DOT  , KC_P  , KC_Y  ,
+        KC_F  , KC_G  , KC_C  , KC_R  , KC_L  ,KC_RBRC,
+        // Home
+        KC_MINS, HM_A  , HM_O  , HM_E  , HM_U  , KC_I  ,
+        KC_LEFT, KC_DOWN, KC_UP, KC_RGHT  ,HM_S,KC_SLASH,
+        // bottom
+        KC_EQL, KC_SCLN, KC_Q  , KC_J  , KC_K  ,KC_X  ,
+        KC_B  , KC_M  ,KC_W,KC_V ,KC_Z,KC_BSLASH,
+        // thumb upper
+        KC_LEFT, KC_RIGHT, KC_ESC,KC_SPC, KC_LBRC, KC_UNDS,
+        KC_UNDS, KC_DEL, KC_BSPC, KC_ENT, KC_DOWN, KC_UP,
         // thumb lower
         KC_LBRC, KC_MINS,
         KC_MINS, LCTL_T(KC_DEL)
