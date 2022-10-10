77eb9733debb9379b478fb8ff08859f1393ca1b1
Bhanuka <bhanukamax@gmail.com>
2022-10-10 19:42:42 +0530

    Dactyl: added transparent keys on nav layer

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/bmax/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/bmax/keymap.c
@@ -2,7 +2,8 @@
 
 enum layers {
   _DVORAK,
-  _NAV
+  _NAV,
+  _NUMBER
 };
 
 #define HM_A LSFT_T(KC_A)
@@ -41,22 +42,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                            ),
       [_NAV] = LAYOUT_5x6(
                            // number
-        KC_TRNS , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,
+        _______ , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,
         KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10  ,KC_F11,
         // top
-        KC_TAB , KC_QUOT  , KC_COMM  , KC_DOT  , KC_P  , KC_Y  ,
-        KC_HOME, KC_PGDN, KC_PGUP, KC_END  , KC_L  ,KC_F12,
+        _______ , _______  , _______  , _______  , _______  , _______  ,
+        KC_HOME, KC_PGDN, KC_PGUP, KC_END  , _______  ,KC_F12,
         // Home
-        KC_MINS, HM_A  , HM_O  , HM_E  , HM_U  , KC_I  ,
-        KC_LEFT, KC_DOWN, KC_UP, KC_RGHT  ,HM_S,KC_SLASH,
+        _______, _______  , _______  , _______  , _______  , _______  ,
+        KC_LEFT, KC_DOWN, KC_UP, KC_RGHT  ,_______, _______,
         // bottom
-        KC_EQL, KC_SCLN, KC_Q  , KC_J  , KC_K  ,KC_X  ,
-        KC_B  , KC_M  ,KC_W,KC_V ,KC_Z,KC_BSLASH,
+        _______, _______, _______  , _______  , _______  ,_______  ,
+        _______  , _______  ,_______,_______ ,_______,_______,
         // thumb upper
-        KC_LEFT, KC_RIGHT, KC_ESC,KC_SPC, KC_LBRC, KC_UNDS,
-        KC_UNDS, KC_DEL, KC_BSPC, KC_ENT, KC_DOWN, KC_UP,
+        _______, _______, _______,_______, _______, _______,
+        _______, _______, _______, _______, _______, _______,
         // thumb lower
-        KC_LBRC, KC_MINS,
-        KC_MINS, LCTL_T(KC_DEL)
+        _______, _______,
+        _______, _______
     )
 };
