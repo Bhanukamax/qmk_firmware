482f1682ac66b61315d8a5f4bffa96384d888b41
Bhanuka <bhanukamax@gmail.com>
2022-10-10 19:46:16 +0530

    dactyl: indentation

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/bmax/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/bmax/keymap.c
@@ -20,44 +20,44 @@ enum layers {
 #define NAV MO(_NAV)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_DVORAK] = LAYOUT_5x6(
-                           // number
-        KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,
-        KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_LBRC,
-        // top
-        KC_TAB , KC_QUOT  , KC_COMM  , KC_DOT  , KC_P  , KC_Y  ,
-        KC_F  , KC_G  , KC_C  , KC_R  , KC_L  ,KC_RBRC,
-        // Home
-        KC_MINS, HM_A  , HM_O  , HM_E  , HM_U  , KC_I  ,
-        KC_D  , HM_H  , HM_T  , HM_N  ,HM_S,KC_SLASH,
-        // bottom
-        KC_EQL, KC_SCLN, KC_Q  , KC_J  , KC_K  ,KC_X  ,
-        KC_B  , KC_M  ,KC_W,KC_V ,KC_Z,KC_BSLASH,
-        // thumb upper
-        KC_LEFT, KC_RIGHT, KC_ESC,KC_SPC, NAV, KC_UNDS,
-        KC_UNDS, KC_DEL, KC_BSPC, KC_ENT, KC_DOWN, KC_UP,
-        // thumb lower
-        KC_SPC, KC_MINS,
-        CTL_DEL, KC_BSPC
-                           ),
-      [_NAV] = LAYOUT_5x6(
-                           // number
-        _______ , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,
-        KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10  ,KC_F11,
-        // top
-        _______ , _______  , _______  , _______  , _______  , _______  ,
-        KC_HOME, KC_PGDN, KC_PGUP, KC_END  , _______  ,KC_F12,
-        // Home
-        _______, _______  , _______  , _______  , _______  , _______  ,
-        KC_LEFT, KC_DOWN, KC_UP, KC_RGHT  ,_______, _______,
-        // bottom
-        _______, _______, _______  , _______  , _______  ,_______  ,
-        _______  , _______  ,_______,_______ ,_______,_______,
-        // thumb upper
-        _______, _______, _______,_______, _______, _______,
-        _______, _______, _______, _______, _______, _______,
-        // thumb lower
-        _______, _______,
-        _______, _______
-    )
+  [_DVORAK] = LAYOUT_5x6(
+                         // number
+                         KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,
+                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_LBRC,
+                         // top
+                         KC_TAB , KC_QUOT  , KC_COMM  , KC_DOT  , KC_P  , KC_Y  ,
+                         KC_F  , KC_G  , KC_C  , KC_R  , KC_L  ,KC_RBRC,
+                         // Home
+                         KC_MINS, HM_A  , HM_O  , HM_E  , HM_U  , KC_I  ,
+                         KC_D  , HM_H  , HM_T  , HM_N  ,HM_S,KC_SLASH,
+                         // bottom
+                         KC_EQL, KC_SCLN, KC_Q  , KC_J  , KC_K  ,KC_X  ,
+                         KC_B  , KC_M  ,KC_W,KC_V ,KC_Z,KC_BSLASH,
+                         // thumb upper
+                         KC_LEFT, KC_RIGHT, KC_ESC,KC_SPC, NAV, KC_UNDS,
+                         KC_UNDS, KC_DEL, KC_BSPC, KC_ENT, KC_DOWN, KC_UP,
+                         // thumb lower
+                         KC_SPC, KC_MINS,
+                         CTL_DEL, KC_BSPC
+                         ),
+  [_NAV] = LAYOUT_5x6(
+                      // number
+                      _______ , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,
+                      KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10  ,KC_F11,
+                      // top
+                      _______ , _______  , _______  , _______  , _______  , _______  ,
+                      KC_HOME, KC_PGDN, KC_PGUP, KC_END  , _______  ,KC_F12,
+                      // Home
+                      _______, _______  , _______  , _______  , _______  , _______  ,
+                      KC_LEFT, KC_DOWN, KC_UP, KC_RGHT  ,_______, _______,
+                      // bottom
+                      _______, _______, _______  , _______  , _______  ,_______  ,
+                      _______  , _______  ,_______,_______ ,_______,_______,
+                      // thumb upper
+                      _______, _______, _______,_______, _______, _______,
+                      _______, _______, _______, _______, _______, _______,
+                      // thumb lower
+                      _______, _______,
+                      _______, _______
+                      )
 };
