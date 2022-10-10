62588337f89f24cad348588c1b3ca578555a1a62
Bhanuka <bhanukamax@gmail.com>
2022-10-10 16:45:44 +0530

    dactyl: format

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/bmax/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/bmax/keymap.c
@@ -20,11 +20,23 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x6(
-        KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,                         KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_LBRC,
-        KC_TAB , KC_QUOT  , KC_COMM  , KC_DOT  , KC_P  , KC_Y  ,                         KC_F  , KC_G  , KC_C  , KC_R  , KC_L  ,KC_RBRC,
-        KC_MINS, HM_A  , HM_O  , HM_E  , HM_U  , KC_I  ,                         KC_D  , HM_H  , HM_T  , HM_N  ,HM_S,KC_SLASH,
-        KC_EQL, KC_SCLN, KC_Q  , KC_J  , KC_K  ,KC_X  ,                         KC_B  , KC_M  ,KC_W,KC_V ,KC_Z,KC_BSLASH,
-        KC_LEFT, KC_RIGHT, KC_ESC,KC_SPC, KC_LBRC, KC_UNDS,                     KC_UNDS, KC_DEL, KC_BSPC, KC_ENT, KC_UP, KC_DOWN,
-        KC_LBRC, KC_MINS,  KC_MINS, LCTL_T(KC_DEL)
+                           // number
+        KC_GRV , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,
+        KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,KC_LBRC,
+        // top
+        KC_TAB , KC_QUOT  , KC_COMM  , KC_DOT  , KC_P  , KC_Y  ,
+        KC_F  , KC_G  , KC_C  , KC_R  , KC_L  ,KC_RBRC,
+        // Home
+        KC_MINS, HM_A  , HM_O  , HM_E  , HM_U  , KC_I  ,
+        KC_D  , HM_H  , HM_T  , HM_N  ,HM_S,KC_SLASH,
+        // bottom
+        KC_EQL, KC_SCLN, KC_Q  , KC_J  , KC_K  ,KC_X  ,
+        KC_B  , KC_M  ,KC_W,KC_V ,KC_Z,KC_BSLASH,
+        // thumb upper
+        KC_LEFT, KC_RIGHT, KC_ESC,KC_SPC, KC_LBRC, KC_UNDS,
+        KC_UNDS, KC_DEL, KC_BSPC, KC_ENT, KC_UP, KC_DOWN,
+        // thumb lower
+        KC_LBRC, KC_MINS,
+        KC_MINS, LCTL_T(KC_DEL)
     )
 };
