2f9ff4fcc78beb96ddb03518a3a85dadf1f19ac2
Bhanuka <bhanukamax@gmail.com>
2022-10-11 20:31:15 +0530

    planck grooming

--- keyboards/planck/keymaps/bmax/keymap.c
+++ keyboards/planck/keymaps/bmax/keymap.c
@@ -19,38 +19,62 @@
 
 
 enum planck_layers {
-  _DVORAK
+  _DVORAK,
+  _NAV,
+  _NUM,
+  _NUMBER
 };
 
-enum planck_keycodes {
-  QWERTY = SAFE_RANGE,
-  COLEMAK,
-  DVORAK,
-  PLOVER,
-  BACKLIT,
-  EXT_PLV
-};
+enum planck_keycodes { QWERTY = SAFE_RANGE, COLEMAK, DVORAK, PLOVER, BACKLIT, EXT_PLV };
 
 
+#define HM_A LSFT_T(KC_A)
+#define HM_O LCTL_T(KC_O)
+#define HM_E LALT_T(KC_E)
+#define HM_U LGUI_T(KC_U)
+
+#define HM_S RSFT_T(KC_S)
+#define HM_N RCTL_T(KC_N)
+#define HM_T RALT_T(KC_T)
+#define HM_H RGUI_T(KC_H)
+
+#define G_LBRC LGUI_T(KC_LBRC)
+#define G_RBRC LGUI_T(KC_RBRC)
+
+#define AL_MINS LALT_T(KC_MINS)
+#define AL_EQL RALT_T(KC_EQL)
+
+#define CTL_DEL LCTL_T(KC_DEL)
+#define NAV MO(_NAV)
+#define NUM_BSPC LT(_NUM, KC_BSPC)
+#define CTL_SPC LCTL_T(KC_SPC)
+#define SHFT_TAB LSFT_T(KC_TAB)
+#define NAV_ESC LT(_NAV, KC_ESC)
 
 #define __ _______
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Dvorak
- * ,-----------------------------------------------------------------------------------.
- * | Tab  |   '  |   ,  |   .  |   P  |   Y  |   F  |   G  |   C  |   R  |   L  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  |   A  |   O  |   E  |   U  |   I  |   D  |   H  |   T  |   N  |   S  |  /   |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Enter |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
- * `-----------------------------------------------------------------------------------'
- */
-[_DVORAK] = LAYOUT_planck_grid(
-KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,   __, __, KC_F,    KC_G,    KC_C,    KC_R,    KC_L,
-KC_A,    KC_O,    KC_E,    KC_U,    KC_I,   __, __, KC_D,    KC_H,    KC_T,    KC_N,    KC_S,
-KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,   __, __, KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,
-KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,   KC_SPC, __, __, KC_SPC,  KC_SPC,   KC_LEFT, KC_DOWN, KC_UP
-)
+  /* Dvorak
+   * ,-----------------------------------------------------------------------------------.
+   * | Tab  |   '  |   ,  |   .  |   P  |   Y  |   F  |   G  |   C  |   R  |   L  | Bksp |
+   * |------+------+------+------+------+------+------+------+------+------+------+------|
+   * | Esc  |   A  |   O  |   E  |   U  |   I  |   D  |   H  |   T  |   N  |   S  |  /   |
+   * |------+------+------+------+------+------+------+------+------+------+------+------|
+   * | Shift|   ;  |   Q  |   J  |   K  |   X  |   B  |   M  |   W  |   V  |   Z  |Enter |
+   * |------+------+------+------+------+------+------+------+------+------+------+------|
+   * | Brite| Ctrl | Alt  | GUI  |Lower |    Space    |Raise | Left | Down |  Up  |Right |
+   * `-----------------------------------------------------------------------------------'
+   */
+  [_DVORAK] = LAYOUT_planck_grid(
+                       //┌─────────┬─────────┬─────────┬─────────┬─────────┐                            ┌─────────┬─────────┬─────────┬─────────┬─────────┐
+                       KC_QUOT,  KC_COMM,  KC_DOT,   KC_P,     KC_Y,   __, __,                     KC_F,     KC_G,     KC_C,     KC_R,     KC_L,
+                       //├─────────┼─────────┼─────────┼─────────┼─────────┤                            ├─────────┼─────────┼─────────┼─────────┼─────────┤
+                       HM_A,     HM_O,     HM_E,     HM_U,     KC_I,         __, __,               KC_D,     HM_H,     HM_T,     HM_N,     HM_S,
+                       //├─────────┼─────────┼─────────┼─────────┼─────────┤                            ├─────────┼─────────┼─────────┼─────────┼─────────┤
+                       KC_SCLN,  KC_Q,     KC_J,     KC_K,     KC_X,    __, __,                     KC_B,     KC_M,     KC_W,     KC_V,     KC_Z,
+                       //├─────────┼─────────┼─────────┼─────────┼─────────┤                            ├─────────┼─────────┼─────────┼─────────┼─────────┤
+                       __,__,__,    CTL_SPC,       KC_SPC,      __,  __,           CTL_DEL,      NUM_BSPC,  __,__,__
+                       //                                    └─────────┘   └─────────┘        └─────────┘   └─────────┘
+                       )
 
 };
