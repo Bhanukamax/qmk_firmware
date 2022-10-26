537baff75ed372bc45be99a8e02b0fd3614d55de
Bhanuka <bhanukamax@gmail.com>
2022-10-26 23:43:57 +0530

    commented dactyl process_record_user function to allow user space one

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/bmax/scripts.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/bmax/scripts.c
@@ -3,44 +3,44 @@
 
 
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-  case LALT_T(C(KC_D)):
-    if (record->tap.count && record->event.pressed) {
-      tap_code16(C(KC_D)); // Send KC_GT on tap
-      return false;        // Return false to ignore further processing of key
-    }
-    break;
-  case LCTL_T(C(KC_S)):
-    if (record->tap.count && record->event.pressed) {
-      tap_code16(C(KC_S)); // Send KC_GT on tap
-      return false;        // Return false to ignore further processing of key
-    }
-    break;
-  case LCTL_T(KC_LPRN):
-    if (record->tap.count && record->event.pressed) {
-      tap_code16(KC_LPRN); // Send KC_GT on tap
-      return false;        // Return false to ignore further processing of key
-    }
-    break;
-  case RCTL_T(KC_RPRN):
-    if (record->tap.count && record->event.pressed) {
-      tap_code16(KC_RPRN); // Send KC_GT on tap
-      return false;        // Return false to ignore further processing of key
-    }
-    break;
-  case LALT_T(KC_LCBR):
-    if (record->tap.count && record->event.pressed) {
-      tap_code16(KC_LCBR); // Send KC_GT on tap
-      return false;        // Return false to ignore further processing of key
-    }
-    break;
-  case RALT_T(KC_RCBR):
-    if (record->tap.count && record->event.pressed) {
-      tap_code16(KC_RCBR); // Send KC_GT on tap
-      return false;        // Return false to ignore further processing of key
-    }
-    break;
-  }
-  return true;
-}
+/* bool process_record_user(uint16_t keycode, keyrecord_t *record) { */
+/*   switch (keycode) { */
+/*   case LALT_T(C(KC_D)): */
+/*     if (record->tap.count && record->event.pressed) { */
+/*       tap_code16(C(KC_D)); // Send KC_GT on tap */
+/*       return false;        // Return false to ignore further processing of key */
+/*     } */
+/*     break; */
+/*   case LCTL_T(C(KC_S)): */
+/*     if (record->tap.count && record->event.pressed) { */
+/*       tap_code16(C(KC_S)); // Send KC_GT on tap */
+/*       return false;        // Return false to ignore further processing of key */
+/*     } */
+/*     break; */
+/*   case LCTL_T(KC_LPRN): */
+/*     if (record->tap.count && record->event.pressed) { */
+/*       tap_code16(KC_LPRN); // Send KC_GT on tap */
+/*       return false;        // Return false to ignore further processing of key */
+/*     } */
+/*     break; */
+/*   case RCTL_T(KC_RPRN): */
+/*     if (record->tap.count && record->event.pressed) { */
+/*       tap_code16(KC_RPRN); // Send KC_GT on tap */
+/*       return false;        // Return false to ignore further processing of key */
+/*     } */
+/*     break; */
+/*   case LALT_T(KC_LCBR): */
+/*     if (record->tap.count && record->event.pressed) { */
+/*       tap_code16(KC_LCBR); // Send KC_GT on tap */
+/*       return false;        // Return false to ignore further processing of key */
+/*     } */
+/*     break; */
+/*   case RALT_T(KC_RCBR): */
+/*     if (record->tap.count && record->event.pressed) { */
+/*       tap_code16(KC_RCBR); // Send KC_GT on tap */
+/*       return false;        // Return false to ignore further processing of key */
+/*     } */
+/*     break; */
+/*   } */
+/*   return true; */
+/* } */
