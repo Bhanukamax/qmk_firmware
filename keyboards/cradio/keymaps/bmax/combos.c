d1a237d7091e044a21dc3f408db959b6d73fb1e0
Bhanuka <bhanukamax@gmail.com>
2022-10-26 23:42:56 +0530

    setup user spoce process_record_user function

--- keyboards/cradio/keymaps/bmax/combos.c
+++ keyboards/cradio/keymaps/bmax/combos.c
@@ -2,42 +2,12 @@
 #include "muse.h"
 #include "keycodes.h"
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case LSFT_T(KC_GT):
-            if (record->tap.count && record->event.pressed) {
-                tap_code16(KC_GT); // Send KC_GT on tap
-                return false;        // Return false to ignore further processing of key
-            }
-            break;
-       case LGUI_T(KC_RCBR):
-          if (record->tap.count && record->event.pressed) {
-                tap_code16(KC_RCBR); // Send KC_GT on tap
-                return false;        // Return false to ignore further processing of key
-            }
-      break;
-       case LALT_T(KC_LCBR):
-          if (record->tap.count && record->event.pressed) {
-                tap_code16(KC_LCBR); // Send KC_GT on tap
-                return false;        // Return false to ignore further processing of key
-            }
-      break;
-        case RALT_T(KC_RCBR):
-          if (record->tap.count && record->event.pressed) {
-                tap_code16(KC_RCBR); // Send KC_GT on tap
-                return false;        // Return false to ignore further processing of key
-            }
-      break;
-    }
-    return true;
-}
 
-
-bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
-  switch(keycode) {
-    case SHIFT_SPACE:
-        return true;
-    default:
-      return false;
-  }
-}
+/* bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) { */
+/*   switch(keycode) { */
+/*     case SHIFT_SPACE: */
+/*         return true; */
+/*     default: */
+/*       return false; */
+/*   } */
+/* } */
