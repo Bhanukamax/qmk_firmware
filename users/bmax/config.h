2210dfdf1eb21da9162999b71e1022d2755a9ece
Bhanuka <bhanukamax@gmail.com>
2022-10-27 20:37:53 +0530

    reformat keymaps

--- users/bmax/config.h
+++ users/bmax/config.h
@@ -5,7 +5,6 @@
 #define _NUM 3
 #define  _FN 4
 
-
 #define HM_A LSFT_T(KC_A)
 #define HM_O LCTL_T(KC_O)
 #define HM_E LALT_T(KC_E)
@@ -44,55 +43,47 @@
 #define XX _______
 
 #define ____DVORAK_L1____ KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y
-#define ____DVORAK_R1____  KC_F, KC_G, KC_C, KC_R, KC_L
+#define ____DVORAK_L2____ HM_A,    HM_O,    HM_E,   HM_U, KC_I
+#define ____DVORAK_L3____ KC_SCLN, KC_Q,    KC_J,   KC_K, KC_X
+#define __DVORAK_MOD_L__  NAV_ESC, KC_SPC
+
+#define ____DVORAK_R1____ KC_F,       KC_G,   KC_C, KC_R, KC_L
+#define ____DVORAK_R2____ KC_D,       HM_H,   HM_T, HM_N, HM_S
+#define ____DVORAK_R3____ KC_B,       KC_M,   KC_W, KC_V, KC_Z
+#define __DVORAK_MOD_R__  NUM_BSPC,   KC_ENT
 
 
-#define ____DVORAK_L2____ HM_A, HM_O, HM_E, HM_U, KC_I
-#define ____DVORAK_R2____ KC_D, HM_H, HM_T, HM_N, HM_S
+#define ____NAV_L1____ XX,      KC_PGUP,         KC_PGDN,         KC_ENT,  XX
+#define ____NAV_L2____ KC_LSFT, LCTL_T(C(KC_S)), LALT_T(C(KC_D)), XX,      XX
+#define ____NAV_L3____ C(KC_Z), C(KC_X),         C(KC_C),         C(KC_V), XX
+#define __NAV_MOD_L__  XX,      XX
 
-#define ____DVORAK_L3____ KC_SCLN, KC_Q, KC_J, KC_K, KC_X
-#define ____DVORAK_R3____ KC_B, KC_M, KC_W, KC_V, KC_Z
-
-#define __DVORAK_MOD_L__ NAV_ESC, KC_SPC
-#define __DVORAK_MOD_R__ NUM_BSPC, KC_ENT
-
-
-#define ____NAV_L1____ XX, KC_PGUP, KC_PGDN, KC_ENT, XX
-#define ____NAV_R1____ KC_HOME, KC_PGDN, KC_PGUP, KC_END, XX
-
-#define ____NAV_L2____ KC_LSFT, LCTL_T(C(KC_S)), LALT_T(C(KC_D)), XX, XX
-#define ____NAV_R2____ KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_BSPC
-
-#define ____NAV_L3____ C(KC_Z), C(KC_X), C(KC_C), C(KC_V), XX
-#define ____NAV_R3____ XX, KC_RBRC, KC_RCBR, KC_RPRN, S(KC_DOT)
-
-#define __NAV_MOD_L__ XX, XX
-#define __NAV_MOD_R__ KC_BSPC, MO(_FN)
+#define ____NAV_R1____ KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XX
+#define ____NAV_R2____ KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC
+#define ____NAV_R3____ XX,      KC_RBRC, KC_RCBR, KC_RPRN, S(KC_DOT)
+#define __NAV_MOD_R__  KC_BSPC, MO(_FN)
 
 
-#define ____NUM_L1____ KC_1, KC_2, KC_3, KC_4, KC_5
-#define ____NUM_R1____ KC_6, KC_7, KC_8, KC_9, KC_0
-
+#define ____NUM_L1____ KC_1,     KC_2,            KC_3,            KC_4,            KC_5
 #define ____NUM_L2____ SHFT_TAB, LCTL_T(KC_LPRN), LALT_T(KC_LCBR), LGUI_T(KC_LBRC), KC_SLASH
-#define ____NUM_R2____ KC_BSLASH, LGUI_T(KC_RBRC), RALT_T(KC_RCBR), RCTL_T(KC_RPRN), RSFT_T(KC_GRV)
+#define ____NUM_L3____ KC_PLUS,  KC_EQL,          KC_UNDS,         KC_MINUS,        S(KC_SLASH)
+#define __NUM_MOD_L__  XX,       XX
 
-#define ____NUM_L3____ KC_PLUS, KC_EQL, KC_UNDS, KC_MINUS, S(KC_SLASH)
-#define ____NUM_R3____ S(KC_BSLASH), S(KC_5), S(KC_COMM), S(KC_DOT), S(KC_2)
+#define ____NUM_R1____ KC_6,         KC_7,            KC_8,            KC_9,            KC_0
+#define ____NUM_R2____ KC_BSLASH,    LGUI_T(KC_RBRC), RALT_T(KC_RCBR), RCTL_T(KC_RPRN), RSFT_T(KC_GRV)
+#define ____NUM_R3____ S(KC_BSLASH), S(KC_5),         S(KC_COMM),      S(KC_DOT),       S(KC_2)
+#define __NUM_MOD_R__  XX,           XX
 
-#define __NUM_MOD_L__ XX, XX
-#define __NUM_MOD_R__ XX, XX
 
 
-#define ____FN_L1____ KC_F1, KC_F2, KC_F3, KC_F4, KC_F5
-#define ____FN_R1____ KC_F6, KC_F7, KC_F8, KC_F9, KC_F10
 
-#define ____FN_L2____ KC_F1, KC_F2, KC_F3, KC_F4, KC_F5
+#define ____FN_L1____ KC_F1, KC_F2, KC_F3, KC_F4,  KC_F5
 #define ____FN_R2____ KC_F6, KC_F7, KC_F8, KC_F11, KC_F12
-
-
-#define ____FN_L3____ KC_F1, KC_F2, KC_F3, KC_F4, KC_F5
 #define ____FN_R3____ KC_F6, KC_F7, KC_F8, KC_F11, KC_F12
 
+#define ____FN_R1____ KC_F6, KC_F7, KC_F8, KC_F9, KC_F10
+#define ____FN_L2____ KC_F1, KC_F2, KC_F3, KC_F4, KC_F5
+#define ____FN_L3____ KC_F1, KC_F2, KC_F3, KC_F4, KC_F5
 
 
 #define LAYOUT_bmax_wrapper(...) LAYOUT_split_3x5_2(__VA_ARGS__)
