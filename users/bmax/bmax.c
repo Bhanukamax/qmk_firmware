01accab2795e549dfde342ebb56b3e3c18773731
Bhanuka <bhanukamax@gmail.com>
2023-01-06 21:15:14 +0530

    rename mocro

--- users/bmax/bmax.c
+++ users/bmax/bmax.c
@@ -1,12 +1,7 @@
 #include QMK_KEYBOARD_H
 #include "bmax.h"
 
-#define ONE RCTL_T(S(KC_LBRC))
-#define ONE_KEY S(KC_LBRC)
-#define TWO RSFT_T(S(KC_RBRC))
-#define TWO_KEY S(KC_RBRC)
-
-#define HANDLE_KEY_CASE(foo, bar)                                              \
+#define CASE_MOD_TAP(foo, bar)                                                 \
   case foo(bar):                                                               \
     if (record->tap.count && record->event.pressed) {                          \
       tap_code16(bar);                                                         \
@@ -14,20 +9,21 @@
     }                                                                          \
     break;
 
-__attribute__((weak)) bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+__attribute__((weak)) bool process_record_user(uint16_t keycode,
+                                               keyrecord_t* record) {
   switch (keycode) {
-    HANDLE_KEY_CASE(RCTL_T, S(KC_LBRC))
-    HANDLE_KEY_CASE(RSFT_T, S(KC_RBRC))
-    HANDLE_KEY_CASE(LALT_T, C(KC_D))
-    HANDLE_KEY_CASE(LCTL_T, C(KC_S))
-    HANDLE_KEY_CASE(RCTL_T, KC_LPRN)
-    HANDLE_KEY_CASE(RCTL_T, KC_RPRN)
-    HANDLE_KEY_CASE(RGUI_T, KC_LPRN)
-    HANDLE_KEY_CASE(RSFT_T, KC_RPRN)
-    HANDLE_KEY_CASE(RGUI_T, KC_LBRC)
-    HANDLE_KEY_CASE(RALT_T, KC_RBRC)
-    HANDLE_KEY_CASE(LALT_T, S(KC_GRV))
-    HANDLE_KEY_CASE(LGUI_T, S(KC_2))
+    CASE_MOD_TAP(RCTL_T, S(KC_LBRC))
+    CASE_MOD_TAP(RSFT_T, S(KC_RBRC))
+    CASE_MOD_TAP(LALT_T, C(KC_D))
+    CASE_MOD_TAP(LCTL_T, C(KC_S))
+    CASE_MOD_TAP(RCTL_T, KC_LPRN)
+    CASE_MOD_TAP(RCTL_T, KC_RPRN)
+    CASE_MOD_TAP(RGUI_T, KC_LPRN)
+    CASE_MOD_TAP(RSFT_T, KC_RPRN)
+    CASE_MOD_TAP(RGUI_T, KC_LBRC)
+    CASE_MOD_TAP(RALT_T, KC_RBRC)
+    CASE_MOD_TAP(LALT_T, S(KC_GRV))
+    CASE_MOD_TAP(LGUI_T, S(KC_2))
   }
   return true;
 }
