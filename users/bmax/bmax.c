12cf6c38cd6524fd7e35864da48ccceef108c0e7
Bhanuka <bhanukamax@gmail.com>
2023-01-29 23:51:50 +0530

    Removed ';' from the mod tap macro

--- users/bmax/bmax.c
+++ users/bmax/bmax.c
@@ -7,22 +7,22 @@
       tap_code16(bar);                                                         \
       return false;                                                            \
     }                                                                          \
-    break;
+    break
 
 __attribute__((weak)) bool process_record_user(uint16_t keycode,
                                                keyrecord_t* record) {
   switch (keycode) {
-    CASE_MOD_TAP(RCTL_T, KC_LBRC)
-    CASE_MOD_TAP(RSFT_T, KC_RBRC)
-    CASE_MOD_TAP(LALT_T, C(KC_D))
-    CASE_MOD_TAP(LCTL_T, C(KC_S))
-    CASE_MOD_TAP(RCTL_T, KC_LPRN)
-    CASE_MOD_TAP(RCTL_T, KC_RPRN)
-    CASE_MOD_TAP(RSFT_T, KC_RPRN)
-    CASE_MOD_TAP(RGUI_T, KC_LPRN)
-    CASE_MOD_TAP(RALT_T, KC_RPRN)
-    CASE_MOD_TAP(LALT_T, S(KC_GRV))
-    CASE_MOD_TAP(LGUI_T, S(KC_2))
+    CASE_MOD_TAP(RCTL_T, KC_LBRC);
+    CASE_MOD_TAP(RSFT_T, KC_RBRC);
+    CASE_MOD_TAP(LALT_T, C(KC_D));
+    CASE_MOD_TAP(LCTL_T, C(KC_S));
+    CASE_MOD_TAP(RCTL_T, KC_LPRN);
+    CASE_MOD_TAP(RCTL_T, KC_RPRN);
+    CASE_MOD_TAP(RSFT_T, KC_RPRN);
+    CASE_MOD_TAP(RGUI_T, KC_LPRN);
+    CASE_MOD_TAP(RALT_T, KC_RPRN);
+    CASE_MOD_TAP(LALT_T, S(KC_GRV));
+    CASE_MOD_TAP(LGUI_T, S(KC_2));
   }
   return true;
 }
