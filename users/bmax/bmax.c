96483b652f104cea472c9e0f9f5d75338e33e4cc
Bhanuka <bhanukamax@gmail.com>
2023-01-31 22:01:42 +0530

    Implement and Applied home row mode tap case handle macro

--- users/bmax/bmax.c
+++ users/bmax/bmax.c
@@ -9,20 +9,26 @@
     }                                                                          \
     break
 
+#define handle_lhmr(one, two, three, four, five) \
+    CASE_MOD_TAP(LCTL_T, one); \
+    CASE_MOD_TAP(LSFT_T, two); \
+    CASE_MOD_TAP(LALT_T, three); \
+    CASE_MOD_TAP(LGUI_T, four);
+
+#define handle_rhmr(one, two, three, four, five) \
+    CASE_MOD_TAP(RCTL_T, four); \
+    CASE_MOD_TAP(RSFT_T, five); \
+    CASE_MOD_TAP(RALT_T, three); \
+    CASE_MOD_TAP(RGUI_T, two);
+
 __attribute__((weak)) bool process_record_user(uint16_t keycode,
                                                keyrecord_t* record) {
   switch (keycode) {
-    CASE_MOD_TAP(RCTL_T, KC_LBRC);
-    CASE_MOD_TAP(RSFT_T, KC_RBRC);
-    CASE_MOD_TAP(LALT_T, C(KC_D));
-    CASE_MOD_TAP(LCTL_T, C(KC_S));
-    CASE_MOD_TAP(RCTL_T, KC_LPRN);
-    CASE_MOD_TAP(RCTL_T, KC_RPRN);
-    CASE_MOD_TAP(RSFT_T, KC_RPRN);
-    CASE_MOD_TAP(RGUI_T, KC_LPRN);
-    CASE_MOD_TAP(RALT_T, KC_RPRN);
-    CASE_MOD_TAP(LALT_T, S(KC_GRV));
-    CASE_MOD_TAP(LGUI_T, S(KC_2));
+        handle_lhmr(KC_A, KC_O, KC_E, KC_U, KC_I)
+        handle_rhmr(KC_D, KC_H, KC_T, KC_N, KC_S)
+        handle_lhmr(KC_LSFT, C(KC_S), C(KC_D), KC_ENT,  S(KC_6))
+        handle_lhmr(SHFT_TAB, KC_GRV, S(KC_GRV), S(KC_2), KC_SLASH)
+        handle_rhmr(KC_BSLASH, KC_LPRN, KC_RPRN,  KC_LBRC,    KC_RBRC)
   }
   return true;
 }
