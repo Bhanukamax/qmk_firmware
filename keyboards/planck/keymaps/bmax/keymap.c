2939450d124ba7d21602a922e271b0eb0a8b9e12
Bhanuka <bhanukamax@gmail.com>
2023-02-11 21:14:22 +0530

    Changed planck back to use the split layout

--- keyboards/planck/keymaps/bmax/keymap.c
+++ keyboards/planck/keymaps/bmax/keymap.c
@@ -25,10 +25,10 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DVORAK] = LAYOUT_planck_grid_wrapper( // Dvorak
-                                          ____DVORAK_L1____, XX, ____DVORAK_R1____, KC_BSPC,
-                                          ____DVORAK_L2____, XX, ____DVORAK_R2____, KC_ENT,
-                                          ____DVORAK_L3____, XX, ____DVORAK_R3____, KC_SLASH,
-                                          RGB_TOG,RGB_MOD, MO(_MUS), __DVORAK_MOD_L__, __DVORAK_MOD_R__p, MO(7), RGB_SAI, RGB_SAD, XX
+                                          ____DVORAK_L1____, XX,XX , ____DVORAK_R1____, // KC_BSPC,
+                                          ____DVORAK_L2____, XX,XX , ____DVORAK_R2____, // KC_ENT,
+                                          ____DVORAK_L3____, XX,XX , ____DVORAK_R3____, // KC_SLASH,
+                                          RGB_TOG,RGB_MOD, MO(_MUS), __DVORAK_MOD_L__, XX, XX,  __DVORAK_MOD_R__, MO(7), RGB_SAI, RGB_SAD //, XX
                                           // RGB_TOG,RGB_MOD, RGB_HUI, __DVORAK_MOD_L__, __, __DVORAK_MOD_R__, RGB_HUD, RGB_SAI, RGB_SAD
                                           ),
 
@@ -42,28 +42,28 @@ ____PLANCK_GROW_R___,
                                           ),
 
   [_NUM] = LAYOUT_planck_grid_wrapper(
-                                      ____NUM_L1____, XX,  ____NUM_R1____, XX,
-                                      ____NUM_L2____, XX,  ____NUM_R2____, XX,
-                                      ____NUM_L3____, XX,  ____NUM_R3____, XX,
-                                      ___, __NUM_MOD_L__ ,  __NUM_MOD_R__p, XX, RGB_VAI, RGB_VAD, XX
+                                      ____NUM_L1____, XX, XX,  ____NUM_R1____, // XX,
+                                      ____NUM_L2____, XX, XX,  ____NUM_R2____, // XX,
+                                      ____NUM_L3____, XX, XX,  ____NUM_R3____, // XX,
+                                      ___, __NUM_MOD_L__ , XX, XX,  __NUM_MOD_R__, XX, RGB_VAI, RGB_VAD// , XX
                                       ),
 
   [_NAV] = LAYOUT_planck_grid_wrapper( // Nav layer
-                                       ____NAV_L1____, XX, ____NAV_R1____, XX,
-                                       ____NAV_L2____, XX, ____NAV_R2____, XX,
-                                       ____NAV_L3____, XX, ____NAV_R3____, XX,
-                                      ___, __NAV_MOD_L__ ,  __NAV_MOD_R__p, XX, RGB_VAI, RGB_VAD,XX
+                                       ____NAV_L1____, XX, XX, ____NAV_R1____, // XX,
+                                       ____NAV_L2____, XX, XX, ____NAV_R2____, // XX,
+                                       ____NAV_L3____, XX, XX, ____NAV_R3____, // XX,
+                                      ___, __NAV_MOD_L__ , XX, XX,  __NAV_MOD_R__, XX, RGB_VAI, RGB_VAD//  ,XX
                                       ),
   [_MUS] = LAYOUT_planck_grid_wrapper( // Mouse layer
-                                       ____MUS_L1____, XX, ____MUS_R1____, XX,
-                                       ____MUS_L2____, XX, ____MUS_R2____, XX,
-                                       ____MUS_L3____, XX, ____MUS_R3____, XX,
+                                       ____MUS_L1____, XX,XX, ____MUS_R1____, //  XX,
+                                       ____MUS_L2____, XX,XX, ____MUS_R2____, //  XX,
+                                       ____MUS_L3____, XX,XX, ____MUS_R3____, //  XX,
                                        ___, XX, XX, __, XX, XX, ___),
 
   [_FN] = LAYOUT_planck_grid_wrapper( // Nav layer
-                                      ____FN_L1____, XX, ____FN_R1____, XX,
-                                      ____FN_L2____, XX, ____FN_R2____, XX,
-                                      ____FN_L3____, XX, ____FN_R3____, XX,
+                                      ____FN_L1____, XX, XX, ____FN_R1____, // XX,
+                                      ____FN_L2____, XX, XX, ____FN_R2____, // XX,
+                                      ____FN_L3____, XX, XX, ____FN_R3____, // XX,
                                       QK_BOOT, QK_RBT, QK_MAKE, KC_TRNS, KC_TRNS, XX, KC_BSPC, MO(_FN), ___, XX)
   // [_PLANCK_NAV] = LAYOUT_planck_grid_wrapper( // Dvorak
   //                                         ___DVORAK_L1____,__,____DVORAK_R1____,
