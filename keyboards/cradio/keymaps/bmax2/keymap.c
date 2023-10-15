4169978bc9b7e970c944af4c8700d61e11b771a3
Don Bhanuka <bhanukamax@gmail.com>
2023-10-15 11:29:51 +0530

    fix

--- keyboards/cradio/keymaps/bmax2/keymap.c
+++ keyboards/cradio/keymaps/bmax2/keymap.c
@@ -22,11 +22,46 @@
 /* Base layer 0 layout uses home row mods. See the following guide for details:
  * https://precondition.github.io/home-row-mods
  */
-const uint16_t PROGMEM test_combo1[] = {KC_A, KC_B, COMBO_END};
-const uint16_t PROGMEM test_combo2[] = {KC_C, KC_D, COMBO_END};
+#define CASE_MOD_TAP(foo, bar)                                                 \
+  case foo(bar):                                                               \
+    if (record->tap.count && record->event.pressed) {                          \
+      tap_code16(bar);                                                         \
+      return false;                                                            \
+    }                                                                          \
+    break
+
+#define handle_lhmr(one, two, three, four, five) \
+    CASE_MOD_TAP(LCTL_T, one); \
+    CASE_MOD_TAP(LSFT_T, two); \
+    CASE_MOD_TAP(LALT_T, three); \
+    CASE_MOD_TAP(LGUI_T, four)
+
+#define handle_rhmr(one, two, three, four, five) \
+    CASE_MOD_TAP(RCTL_T, four); \
+    CASE_MOD_TAP(RSFT_T, five); \
+    CASE_MOD_TAP(RALT_T, three); \
+    CASE_MOD_TAP(RGUI_T, two)
+
+__attribute__((weak)) bool process_record_user(uint16_t keycode,
+                                               keyrecord_t* record) {
+  switch (keycode) {
+        handle_lhmr(SHFT_TAB, KC_GRV, S(KC_GRV),KC_UNDS, KC_SLASH);
+        handle_rhmr(KC_BSLASH,   KC_UNDS, KC_EQL, KC_LBRC, KC_RBRC);
+  }
+  return true;
+}
+
+#define bake_duel_combo(cname, key1, key2) \
+  const uint16_t PROGMEM cname[] = {key1, key2, COMBO_END}
+
+bake_duel_combo(comb_pu,    KC_P,               LGUI_T(KC_U));
+bake_duel_combo(comb_gh,    KC_G,                RGUI_T(KC_H));
+bake_duel_combo(comb_quot_a, LT(_NAV, KC_QUOT),  LSFT_T(KC_A));
+
 combo_t key_combos[] = {
-    COMBO(test_combo1, KC_ESC),
-    COMBO(test_combo2, LCTL(KC_Z)), // keycodes with modifiers are possible too!
+    COMBO(comb_gh,      KC_ENT),
+    COMBO(comb_pu,      KC_ESC),
+    COMBO(comb_quot_a,   KC_TAB),
 };
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DVORAK] = LAYOUT_bmax_wrapper( // Dvorak
